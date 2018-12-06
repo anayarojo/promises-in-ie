67d9e80fdc1d7bab5bfcb62634c0fc335c4b920e
anayarojo <raulmartin_04@hotmail.com>
2018-12-06 14:03:48 -0700

    Implement true or false function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
 
-    var promise = new Promise(function(resolve) {
+    function trueOrFalse(){
+        return Math.random() >= 0.5;
+    };
+
+    var promise = new Promise(function(resolve, reject) {
         setTimeout(function() {
-            resolve("result");
+            if(trueOrFalse()) {
+                resolve("Success");
+            } else {
+                reject("Fail");
+            }
         }, 1000);
     });
     
-    promise.then(function(result) {
-        alert("Success");
-    }, function(error) {
-        alert("Fail");
+    promise.then(function(resp){
+        alert(resp);
+        console.log(resp);
+    }).catch(function(resp){
+        alert(resp);
+        console.error(resp);
     });
 
 });
